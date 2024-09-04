3630cba9c1f9abc78bdd1dd09c4b59022197e661
junahak <163312595+junahak@users.noreply.github.com>
2024-09-04 15:59:22 +0900

    공격하고 점프
    이동은 아직 조이스틱 에셋 받으면 할게요

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,18 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody2D rigid;
+
+    public Transform attackPoint; // 공격 위치
+    public float attackRange = 0.5f; // 공격 범위
+    public LayerMask enemyLayers; // 적 레이어
+    public int attackDamage = 10; // 공격력    
+    public float jumpForce = 10f;
+    public Button jumpButton;
+    public Button attackButton;
+    public Text buttonText;
+   
+    private Rigidbody2D rb;
+
+    public bool isJump = false;
+
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        attackButton.onClick.AddListener(OnAttackButtonClick);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void Jump()
+    {
+        Debug.Log("점프");
+        if (!isJump)
+        {
+            isJump = true;
+            rigid.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Equals("Floor"))
+        {
+            isJump = false;
+        }
+    }
+
+    public void OnJumpButtonClick()
+    {
+        Jump();
+    }
+
+    public void Attack()
+    {
+        Debug.Log("공격");
+        // 공격 범위 내의 적을 탐지
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        
+        // 적에게 피해를 입히는 로직 (예: 적의 Health 컴포넌트에 접근하여 피해를 입힘)
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("공격 범위");
+            // 적의 Health 스크립트에 접근하여 피해를 입히는 코드
+            // 예: enemy.GetComponent<EnemyHealth>().TakeDamage(attackDamage);
+        }
+    }
+
+    public void OnAttackButtonClick()
+    {
+        Attack();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+
 }
